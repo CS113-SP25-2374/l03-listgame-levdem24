4933bcf54636d48b68e4b078516470b5d2929427
MCCLab4804WD23 <levondemirdjian24@gmail.com>
2025-02-18 18:35:31 -0800

    finished part 1.

--- src/main/java/cs113/listGame/resources/ResourceArrayList.java
+++ src/main/java/cs113/listGame/resources/ResourceArrayList.java
@@ -1,25 +1,66 @@
 package cs113.listGame.resources;
+import java.util.ArrayList;
 
 import cs113.listGame.gamecore.GameObject;
 
-public class ResourceArrayList implements ResourceList {
-    @Override
-    public void add(ResourceObject resource) {
 
+public class ResourceArrayList implements ResourceList {
+
+    ArrayList<ResourceObject> resources = new ArrayList(4);
+
+    @Override
+
+    public void add(ResourceObject resource) {
+        if (contains(resource)) {
+            return;
+        } // gotcha moment, don't add it twice!
+
+        for (int i = 0; i < resources.size(); i++) {
+            if (resources.get(i) == null) {
+                resources.set(i, resource);
+                return;
+            }
+        }
+    }
+
+    private boolean contains(ResourceObject resource) {
+        for (int i = 0; i < resources.size(); i++) {
+            if (resources.get(i) == resource) return true;
+        }
+        return false;
     }
 
     @Override
     public void remove(ResourceObject resource) {
+        for (int i = 0; i < resources.size(); i++) {
+            if (resources.get(i) == resource) {
+                resources.set(i, null);
+            }
+        }
+
+
+    }
+    @Override
+    public void truncate(ResourceObject resource){
+        boolean found = false;
+        for (int i = 0; i < resources.size(); i++) {
+            if (resources.get(i) == resource) {
+                found = true;
+            }
+            if (found) {
+                resources.set(i, null);
+            }
+        }
 
     }
 
     @Override
-    public void truncate(ResourceObject resource) {
-
-    }
-
-    @Override
-    public void follow(GameObject leader) {
-
+    public void follow (GameObject leader){
+        for (int i = 0; i < resources.size(); i++) {
+            if (resources.get(i) != null) {
+                resources.get(i).moveTowards(leader.getEchoCenter());
+                leader = resources.get(i);
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module cs113.listGame {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens cs113.listGame to javafx.fxml;
